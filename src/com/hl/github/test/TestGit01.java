ec8c3ab1f3f1d50b87c4fd5eac3b904ba59899e5
huanglong1218 <1156247345@qq.com>
2017-04-06 04:23:29 +0800

    update from yuancheng
    update from yuancheng

--- src/com/hl/github/test/TestGit01.java
+++ src/com/hl/github/test/TestGit01.java
@@ -2,8 +2,9 @@ package com.hl.github.test;
 
 public class TestGit01 {
 	public static void main(String[] args) {
-		System.out.println("hello,github服务器!");
-		System.out.println("hello,github服务器!02");
-		System.out.println("hello,github服务器!03");
+		System.out.println("hello,github服务器!from yuancheng");
+		System.out.println("hello,github服务器!02from yuancheng");
+		System.out.println("hello,github服务器!03from yuancheng");
+		System.out.println("from yuancheng");
 	}
 }
