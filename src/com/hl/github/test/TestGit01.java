dae95030d7aa9c914b81cb254d2913a4df4f943e
huanglong1218 <1156247345@qq.com>
2017-04-06 04:16:01 +0800

    update file 02

--- src/com/hl/github/test/TestGit01.java
+++ src/com/hl/github/test/TestGit01.java
@@ -3,5 +3,6 @@ package com.hl.github.test;
 public class TestGit01 {
 	public static void main(String[] args) {
 		System.out.println("hello,github服务器!");
+		System.out.println("hello,github服务器!");
 	}
 }
