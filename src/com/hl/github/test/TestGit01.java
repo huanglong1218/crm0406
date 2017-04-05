c588237cfad00bb569a0d69594814e57b33a208f
huanglong1218 <1156247345@qq.com>
2017-04-06 04:19:29 +0800

    update 03

--- src/com/hl/github/test/TestGit01.java
+++ src/com/hl/github/test/TestGit01.java
@@ -3,6 +3,7 @@ package com.hl.github.test;
 public class TestGit01 {
 	public static void main(String[] args) {
 		System.out.println("hello,github服务器!");
-		System.out.println("hello,github服务器!");
+		System.out.println("hello,github服务器!02");
+		System.out.println("hello,github服务器!03");
 	}
 }
